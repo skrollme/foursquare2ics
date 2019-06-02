8d9bde6f8d17207b8bc510445379bd8ef7c99de7
Sebastian Kroll <info@skroll.me>
2019-06-02 18:00:42 +0200

    reformatting

--- src/fetch.php
+++ src/fetch.php
@@ -13,12 +13,12 @@
     $foursquare = new FoursquareApi(FOURSQUARE_CLIENTID, FOURSQUARE_CLIENTSECRET);
     $foursquare->SetAccessToken(FOURSQUARE_OAUTHTOKEN);
     $params = array(
-        "limit" => 100,
-        "offset" => 0
+        'limit' => 100,
+        'offset' => 0
     );
     $data = $foursquare->GetPrivate('users/' .FOURSQUARE_USERID. '/checkins',$params);
 
-    if($data = json_decode($data)) {
+    if($data = json_decode($data,false)) {
         if ($data->meta->code == '200' && isset($data->response->checkins->items)) {
             foreach ($data->response->checkins->items AS $item) {
                 if (!$resItemExists = mysqli_query($db, sprintf("SELECT * FROM 4sq2ics_checkins WHERE id = '%s'", $item->id))) {
@@ -26,29 +26,29 @@
                 }
 
                 if ($resItemExists->num_rows !== 0) {
-                    echo $item->id . " found, skipping\n";
+                    echo sprintf("%s found, skipping\n", $item->id);
                     continue;
                 }
 
-                echo $item->id . " adding, ";
+                echo sprintf('%s adding, ', $item->id);
                 $dataset = array(
-                    "id" => $item->id,
-                    "timestamp" => $item->createdAt,
-                    "user" => "1",
-                    "venue" => $item->venue->name,
-                    "lat" => $item->venue->location->lat,
-                    "long" => $item->venue->location->lng,
-                    "country" => $item->venue->location->cc,
-                    "zip" => $item->venue->location->postalCode,
-                    "city" => $item->venue->location->city,
-                    "address" => $item->venue->location->address
+                    'id' => $item->id,
+                    'timestamp' => $item->createdAt,
+                    'user' => '1',
+                    'venue' => $item->venue->name,
+                    'lat' => $item->venue->location->lat,
+                    'long' => $item->venue->location->lng,
+                    'country' => $item->venue->location->cc,
+                    'zip' => $item->venue->location->postalCode,
+                    'city' => $item->venue->location->city,
+                    'address' => $item->venue->location->address
                 );
 
                 foreach ($dataset AS $key => &$col) {
                     $col = addslashes(utf8_decode($col));
                 }
 
-                $sql = sprintf("INSERT INTO 4sq2ics_checkins VALUES (%s)", "'" . implode("','", $dataset) . "'");
+                $sql = sprintf('INSERT INTO 4sq2ics_checkins VALUES (%s)', "'" . implode("','", $dataset) . "'");
                 if ($resAdditem = mysqli_query($db, $sql)) {
                     echo "added\n";
                 } else {
